8c8fc3718064707b2e6ea718bb7873ed3bbab3be
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-05-24 14:49:21 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+<main>
+  <header>
+    <a href='#' className='logo'>My Blog</a>
+    <nav>
+      <a href='#'>Login</a>
+      <a href='#'>Register</a>
+    </nav>
+  </header>
+  <div className='post'>
+    <img src='https://techcrunch.com/wp-content/uploads/2023/02/GettyImages-1240099721.jpeg?w=1390&crop=1'/>
+    <div className='text'>
+    <h2>Netflix begins its password sharing crackdown in the US and global markets</h2>
+    <p>Netflix’s crackdown on password sharing is now beginning to roll out to U.S. subscribers and other global markets, after a delayed launch. The streamer had originally planned to introduce “paid sharing” to U.S. subscribers in the first quarter of this year but pushed the start date back to the summer, after seeing cancellations in markets where it had already la</p>
+    </div>
+    
+  </div>
+  
+  
+</main>
+  );
+}
+
+export default App;
